cd7cb07bb289eed57a099d1675189b83e4df75ae
BudComSol <michael@raymounts.com>
2026-02-15 17:20:02 +1030

    Revise welcome message and update features list
    Updated the welcome message and features list to reflect changes in the game description and added new features for PHP 8.0.

--- home.php
+++ home.php
@@ -2,62 +2,64 @@
 declare(strict_types=1);
 require_once __DIR__.'/inc/nliheader.php';
 ?><tr>
-    <th class="content-head">.: Welcome To <?php echo GAME_NAME; ?>!</th>
+    <th class="content-head">Whats This?</th>
 </tr>
 <tr>
     <td class="content">
         <table width="100%" cellpadding="5" cellspacing="2">
             <tr>
-                <td width="120" class="center"><img src="images/logos/mobster.jpg" border="0" alt="GRPG" /></td>
+                <td class="center img"><img src="images/logos/engine.png" border="0" alt="gRPG Engine Logo" /></td>
                 <td class="center">
-                    Welcome to Generic RPG. GRPG is a Generic Role Playing Game that can be changed to what ever you would like it to be, you play it all in your web browser, and best of all you don't have to pay for anything. In GRPG, you choose your own path. Whether you want to train your stats and become the strongest player, or become the president and actually effect and change aspects of the game, it is entirely up to you.
+                    Welcome to Generic RPG or gRPG which is an engine written in PHP that allows you to create an MMORPG style browser game of your very own. It can be themed in just about any genre, from gangster to outer space, it's limited only by you're imagination and the best part is that it is free and can be found on Github.
+                    
                 </td>
             </tr>
         </table>
     </td>
 </tr>
 <tr>
-    <th class="content-head">.: Features</th>
+    <th class="content-head">Whats It Do?</th>
 </tr>
 <tr>
-    <td class="content">
-        Some of the features that GRPG has are as follows:
+    <td class="content">        
         <ul>
-            <li>Written for PHP 7.4+</li>
-            <li>PDO prepare statements</li>
-            <li>Optional database-driven sessions</li>
-            <li>Cronless Crons</li>
-            <li>RPG-Style leveling system</li>
-            <li>Stats that effect what you can do in the game</li>
-            <li>Mugging and attacking of other players</li>
-            <li>Classified Ads</li>
-            <li>Bank to keep your money safe in</li>
-            <li>Complete gang system</li>
-            <li>Gym to train your stats in</li>
-            <li>Hospital that people are sent to when they get beat up</li>
-            <li>Jail that people go to if captured, but you can be broken out of</li>
-            <li>Private Message System so you can contact other players easily</li>
-            <li>Event system to let you know when something interesting happens, so that you never miss a thing</li>
-            <li>Forums to interact with other players</li>
-            <li>A wiki that explains all the aspects of the game, and is constantly updated by GRPG players</li>
-            <li>RM Store where you can buy in game items and Membership to get ahead in the game using real money</li>
-            <li>Armor and weapons to raise your attack and defense in combat</li>
-            <li>A point system that allows you to recharge your energy and nerve easily</li>
-            <li>Ability to vote on topsites for in game rewards</li>
-            <li>Houses you can buy and live in to gain higher max awake</li>
-            <li>Hall of fame ranking system where you can see the strongest and best players</li>
-            <li>A casino where you can gamble your hard earned cash playing the lottery, the slot machine, or the 50/50 game</li>
-            <li>Job system where you can get a job and make money every day</li>
+            <li>Cronless Crons.</li>            
+            <li>Updated for PHP 8.0+</li>
+            <li>Complete gang system.</li>            
+            <li>PDO prepare statements.</li>
+            <li>Gym to train your stats in.</li>
+            <li>RPG-Style leveling system.</li>            
+            <li>Optional database-driven sessions.</li>                      
+            <li>Stats that effect what you can do in the game.</li>
+            <li>Ability to vote on topsites for in game rewards.</li>            
+            <li>Classified Ads System to promote stuff and maybe earn.</li>
+            <li>Mugging and attacking of other players, thats always fun.</li>
+            <li>Houses you can buy and live in to gain higher max awake.</li>
+            <li>Bank to keep your money safe in, as if you trust banks huh.</li>
+            <li>Jail that people go to if captured, but you can be broken out of.</li>
+            <li>Forums to interact with other players, report issues, help others.</li> 
+            <li>Armor and weapons to raise your attack and defense in combat.</li>
+            <li>Private Message System so you can contact other players easily.</li>                      
+            <li>Hospital that people are sent to when they get themselves beaten up.</li>
+            <li>A point system that allows you to recharge your energy and nerve easily.</li> 
+            <li>Hall of fame ranking system where you can see the strongest and best players.</li> 
+            <li>A wiki that explains all the aspects of the game, and is updated by GRPG players.</li>  
+            <li>A casino where you can gamble your hard earned cash playing the lottery and more.</li>        
+            <li>Event system to let you know when something interesting happens behind your back.</li>            
+            <li>Job system where you can get a job and make money every day and not work to hard.</li>
+            <li>RM Store where you buy in game items and Membership in the game using real money.</li>            
         </ul>
     </td>
 </tr>
 <tr>
-    <th class="content-head">.: Screenshots</th>
+    <th class="content-head">Screenshots</th>
 </tr>
 <tr>
     <td class="content">
         <img src="images/screenshots/screen1.png" width="152" height="152" alt="Screenshots" />
         <img src="images/screenshots/screen2.png" width="152" height="152" alt="Screenshots" />
     </td>
-</tr><?php
-require_once __DIR__.'/inc/nlifooter.php';
+</tr>
+
+<?php require_once __DIR__.'/inc/nlifooter.php'; ?>
+
