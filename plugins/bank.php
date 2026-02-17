f3a67461023df8524954453cdca9229aeacc84f4
BudComSol <michael@raymounts.com>
2026-02-17 20:24:45 +1030

    Update messages and form actions in bank.php

--- plugins/bank.php
+++ plugins/bank.php
@@ -13,7 +13,7 @@ if (array_key_exists('deposit', $_POST)) {
     $db->execute([$_POST['damount'], $_POST['damount'], $user_class->id]);
     $user_class->money -= $_POST['damount'];
     $user_class->bank += $_POST['damount'];
-    echo Message('Money deposited.');
+    echo Message('The Money Has Been Deposited.');
 }
 if (array_key_exists('withdraw', $_POST)) {
     if (!csrf_check('wit_csrf', $_POST)) {
@@ -21,13 +21,13 @@ if (array_key_exists('withdraw', $_POST)) {
     }
     $_POST['wamount'] = array_key_exists('wamount', $_POST) && ctype_digit(str_replace(',', '', $_POST['wamount'])) ? (float)str_replace(',', '', $_POST['wamount']) : $user_class->bank;
     if ($_POST['wamount'] > $user_class->bank) {
-        echo Message('You don\'t have that much money in the bank', 'Error', true);
+        echo Message('You don\'t have that much money in the bank numpty.', 'Error', true);
     }
     $db->query('UPDATE users SET bank = GREATEST(bank - ?, 0), money = money + ? WHERE id = ?');
     $db->execute([$_POST['wamount'], $_POST['wamount'], $user_class->id]);
     $user_class->money += $_POST['wamount'];
     $user_class->bank -= $_POST['wamount'];
-    echo Message('Money withdrawn.');
+    echo Message('The Money Has Been Withdrawn.');
 }
 if (array_key_exists('open', $_GET)) {
     if (!csrf_check('csrfg', $_GET)) {
@@ -39,7 +39,7 @@ if (array_key_exists('open', $_GET)) {
     $db->query('UPDATE users SET whichbank = 1, money = GREATEST(money - 5000, 0) WHERE id = ?');
     $db->execute([$user_class->id]);
     $user_class = new User($user_class->id);
-    echo Message('Your new bank account has been opened and is ready for use');
+    echo Message('<p>Your new bank account has been opened and is ready for use.</p>');
 }
 $interest = $user_class->rmdays > 0 ? .04 : .02;
 $interest = ceil($user_class->bank * $interest);
@@ -47,13 +47,13 @@ $interest = ceil($user_class->bank * $interest);
     <th class="content-head">Bank</th>
 </tr><?php
 if (!$user_class->whichbank) {
-    echo Message('You don\'t currently have an account with us. Would you like to open one for '.prettynum(5000, true).'?<br /><a href="bank.php?open&amp;csrfg='.csrf_create('csrfg', false).'">Yes</a>', 'Open An Account', true);
+    echo Message('<p>You don\'t currently have an account with us.</p> Would you like to open one for '.prettynum(5000, true).'?<br /><a href="plugins/bank.php?open&amp;csrfg='.csrf_create('csrfg', false).'"><p>Yes Please</p></a>', 'Open An Account', true);
 }
 ?><tr>
     <td class="content">
-        Welcome to the bank. You currently have <?php echo prettynum($user_class->bank, true); ?> in your account.<br />
+        <p>Welcome to the bank, you currently have <?php echo prettynum($user_class->bank, true); ?> in your account.</p>
         You will make <?php echo prettynum($interest, true); ?> from interest next rollover.<br /><br />
-        <form action="bank.php" method="post" class="pure-form pure-form-aligned">
+        <form action="plugins/bank.php" method="post" class="pure-form pure-form-aligned">
             <?php echo csrf_create('wit_csrf'); ?>
             <div class="pure-control-group">
                 <label for="wamount">Withdraw</label>
@@ -62,8 +62,9 @@ if (!$user_class->whichbank) {
             <div class="pure-controls">
                 <button type="submit" name="withdraw" class="pure-button pure-button-primary">Withdraw</button>
             </div>
+           <br>
         </form>
-        <form action="bank.php" method="post" class="pure-form pure-form-aligned">
+        <form action="plugins/bank.php" method="post" class="pure-form pure-form-aligned">
             <?php echo csrf_create('dep_csrf'); ?>
             <div class="pure-control-group">
                 <label for="damount">Deposit</label>
