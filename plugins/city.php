758b0cd468414534b3d09257fb8630eca3881441
BudComSol <michael@raymounts.com>
2026-02-26 08:47:05 +1030

    Fix link to Your Gang in city.php

--- plugins/city.php
+++ plugins/city.php
@@ -96,7 +96,7 @@ for ($i = 1; $i <= 3; ++$i) {
             <td class="top" style="padding-bottom:10px;">
                     <h3 style="padding:0;margin:0;font-size:1.4em;">Southside</h3><br />
                     <a href="plugins/gang_list.php">Gangs</a><br />
-                    <a href="<?php echo !$user_class->gang ? 'create' : ''; ?>gang.php">Your Gang</a><br />                    
+                    <a href="<?php echo !$user_class->gang ? 'create' : ''; ?>plugins/gang.php">Your Gang</a><br />                    
                     <a href="plugins/jobs.php">Jobs Center</a><br />                                        
                     <a href="plugins/realestate.php">Real Estate Agency</a>
                 </td>
