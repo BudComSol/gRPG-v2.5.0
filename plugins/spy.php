891638843a5d1299103b47c633427814a335fa2d
BudComSol <michael@raymounts.com>
2026-02-28 15:32:28 +1030

    Improve error messages and output formatting

--- plugins/spy.php
+++ plugins/spy.php
@@ -22,27 +22,27 @@ $spy_class = new User($_GET['id']);
 $cost = $user_class->level * 1000;
 if (array_key_exists('confirm', $_GET)) {
         if ($cost > $user_class->money) {
-            echo Message('You don\'t have enough money', 'Error', true);
+            echo Message('<p>You don\'t have enough money for this, numpty.</p>', 'Error', true);
         }
-        $points = mt_rand(0, 1) == 1 ? format($spy_class->points) : 'Your Private Investigator was unable to find information on their points';
-        $bank = mt_rand(0, 1) == 1 ? format($spy_class->bank) : 'Your Private Investigator was unable to find information on their bank';
-        $strength = mt_rand(0, 1) == 1 ? format($spy_class->strength) : 'Your Private Investigator was unable to find information on their strength';
-        $defense = mt_rand(0, 1) == 1 ? format($spy_class->defense) : 'Your Private Investigator was unable to find information on their defense';
-        $speed = mt_rand(0, 1) == 1 ? format($spy_class->speed) : 'Your Private Investigator was unable to find information on their speed';
+        $points = mt_rand(0, 1) == 1 ? format($spy_class->points) : '<p>Your Private Investigator was unable to find information on their points.</p>';
+        $bank = mt_rand(0, 1) == 1 ? format($spy_class->bank) : '<p>Your Private Investigator was unable to find information on their bank.</p>';
+        $strength = mt_rand(0, 1) == 1 ? format($spy_class->strength) : '<p>Your Private Investigator was unable to find information on their strength.</p>';
+        $defense = mt_rand(0, 1) == 1 ? format($spy_class->defense) : '<p>Your Private Investigator was unable to find information on their defense.</p>';
+        $speed = mt_rand(0, 1) == 1 ? format($spy_class->speed) : '<p>Your Private Investigator was unable to find information on their speed.</p>';
         $db->trans('start');
         $db->query('UPDATE users SET money = GREATEST(money - ?, 0) WHERE id = ?');
         $db->execute([$cost, $user_class->id]);
         $db->query('INSERT INTO spylog (id, spyid, strength, defense, speed, bank, points, age) VALUES (?, ?, ?, ?, ?, ?, ?, ?)');
         $db->execute([$user_class->id, $spy_class->id, $strength, $defense, $speed, $bank, $points, time()]);
-        $db->trans('end'); ?>Your Private Investigator found out the following about <?php echo $spy_class->formattedname; ?><br />
+        $db->trans('end'); ?><p>Your Private Investigator found out the following about <?php echo $spy_class->formattedname; ?></p>
             <strong>Points:</strong> <?php echo $points; ?><br />
             <strong>Bank:</strong> <?php echo $bank; ?><br />
             <strong>Strength:</strong> <?php echo $strength; ?><br />
             <strong>Defense:</strong> <?php echo $defense; ?><br />
             <strong>Speed:</strong> <?php echo $speed;
     } else {
-        ?>Are you sure that you want to hire a Private Investigator to spy on <?php echo $spy_class->formattedname; ?> for <?php echo prettynum($cost, true); ?>?<br />
-                <a href="spy.php?id=<?php echo $spy_class->id; ?>&amp;confirm&amp;csrfg=<?php echo csrf_create('csrfg', false); ?>">Yes</a> | <a href="profiles.php?id=<?php echo $spy_class->id; ?>">No</a><?php
+        ?><p>Are you sure that you want to hire a Private Investigator to spy on <?php echo $spy_class->formattedname; ?> for <?php echo prettynum($cost, true); ?>?</p>
+                <a href="plugins/spy.php?id=<?php echo $spy_class->id; ?>&amp;confirm&amp;csrfg=<?php echo csrf_create('csrfg', false); ?>">Yes</a> | <a href="plugins/profiles.php?id=<?php echo $spy_class->id; ?>">No</a><?php
     }
 ?></td>
 </tr>
