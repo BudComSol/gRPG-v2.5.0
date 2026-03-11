bef5a263da3abe9918cfe22ca73db36318ab4eae
BudComSol <michael@raymounts.com>
2026-03-11 18:47:09 +1030

    Update menu links and fix formatting issues

--- inc/menu.php
+++ inc/menu.php
@@ -56,16 +56,16 @@ $unread_events = $db->result();
     <a class="leftmenu" href="plugins/crime.php">Crime</a>
     <a class="leftmenu" href="plugins/npcs.php">NPCs</a>
     <a class="leftmenu" href="plugins/forum.php">Forum</a>
-    <a class="leftmenu" href="plugins/todo.php">To-Do</a>
-    <a class="leftmenu" href="plugins/events.php">Events <!_-events-_!> [<?php echo $unread_events; ?>]</a>    
+    <a class="leftmenu" href="plugins/todo.php">To-Do</a>        
     <a class="leftmenu" href="plugins/city.php"><!_-cityname-_!></a>        
     <a class="leftmenu" href="plugins/hospital.php">Hospital <!_-hospital-_!></a>
     <a class="leftmenu" href="plugins/inventory.php">Inventory</a>
+    <a class="leftmenu" href="plugins/events.php">Events <!_-events-_!> [<?php echo $unread_events; ?>]</a>
     <a class="leftmenu" href="<?php echo !$user_class->gang ? 'plugins/create' : 'plugins/'; ?>gang.php">Your Gang</a>
     <a class="leftmenu" href="plugins/pms.php">Mailbox <!_-mail-_!> [<?php echo $unread_mail; ?>]</a>
-    <a class="leftmenu" href="plugins/gym.php">Gymnasium</a>        
-    <a class="leftmenu" href="plugins/theatre.php">Theatre</a>
-    <a class="leftmenu" href="plugins/rmstore.php">Game Store</a><?php
+    <a class="leftmenu" href="plugins/gym.php">Gymnasium</a>    
+    <a class="leftmenu" href="plugins/rmstore.php">Game Store</a>
+    <a class="leftmenu" href="plugins/theatre.php">Movie Theatre</a><?php
     if ($user_class->admin == 1) {
         ?>
         <div class="headbox" style="color:yellow;">Staff</div>
