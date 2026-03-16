7bb21d0d78cbf10415cc22ebb6bd031cfbd95d19
BudComSol <michael@raymounts.com>
2026-03-16 12:35:08 +1030

    Fix typo in registration closed message

--- register.php
+++ register.php
@@ -162,7 +162,7 @@ if (defined('CAPTCHA_REGISTRATION') && CAPTCHA_REGISTRATION == true) {
     } else {
         ?>
               <img src="images/headers/rego-closed.png" alt="gRPG Registration Closed">    
-        <p>Registration For The Site Is Currently Closed.</p><p>This Is A Demo Of The Script Found Here <a href="https://github.com/BudComSol/gRPG-v2.5.0" target="_blank" rel="noopener">Github</a> Where You Can Create Your Own Game.</p><?php
+        <p>Registration For The Site Is Currently Closed.</p><p>This Is A Demo Of The Script Found On <a href="https://github.com/BudComSol/gRPG-v2.5.0" target="_blank" rel="noopener">Github</a> Where You Can Create Your Own Game.</p><?php
     } ?>
         
     </td>
